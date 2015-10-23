8b9fceebf12a8dd1aed8b47c6a156f7c6334e954
iaxax <1483809252@qq.com>
2015-10-23 19:30:28 +0800

    重新上传

--- client/src/main/java/driver/bl_driver/deliveryBL_driver/ReceiptBL_Driver.java
+++ client/src/main/java/driver/bl_driver/deliveryBL_driver/ReceiptBL_Driver.java
@@ -1,10 +1,5 @@
 package driver.bl_driver.deliveryBL_driver;
 
-import java.util.Scanner;
-
-import businessLogicService.deliveryBLService.ReceiptBLService;
-import stub.businessLogicImpl_stub.deliveryBLImpl_stub.ReceiptBLImpl_Stub;
-
 public class ReceiptBL_Driver {
 
 }
