caed77edd9d2e7aa65e205bf479f830e6aae6d2a
liumingke <1272072141@qq.com>
2015-10-24 11:45:33 +0800

    gtihub上传第五次
    gtihub上传第五次

--- client/src/main/java/businessLogicImpl/financeBLImpl/CostBLImpl.java
+++ client/src/main/java/businessLogicImpl/financeBLImpl/CostBLImpl.java
@@ -1,7 +1,18 @@
 package businessLogicImpl.financeBLImpl;
 
+import vo.financeVO.PaymentVO;
 import businessLogicService.financeBLService.CostBLService;
+/**
+ * description:为成本管理界面提供服务的具体实现
+ * @author 张仁知
+ *
+ */
 
 public class CostBLImpl implements CostBLService{
+	
+	public boolean addPayment(PaymentVO paymentVO) {
+		
+		return false;
+	}
 
 }
