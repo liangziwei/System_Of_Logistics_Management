1c1ed25d11e3b2fcf3158f55e5cff6c22a89400d
liumingke <1272072141@qq.com>
2015-10-24 16:09:45 +0800

    server BankPO忘记了 已经修改
    server BankPO忘记了 已经修改

--- client/src/main/java/businessLogicImpl/financeBLImpl/SettlementBLImpl.java
+++ client/src/main/java/businessLogicImpl/financeBLImpl/SettlementBLImpl.java
@@ -1,7 +1,27 @@
 package businessLogicImpl.financeBLImpl;
 
+import java.util.List;
+
+import vo.businessVO.ReceivableVO;
 import businessLogicService.financeBLService.SettlementBLService;
+/**
+ * description:为结算管理界面提供服务的具体实现
+ * @author 张仁知
+ *
+ */
 
 public class SettlementBLImpl implements SettlementBLService{
+	
+	public List<ReceivableVO> showReceiList(String date) {
+		return null;
+	}
+	
+	public double calculateSum() {
+		return 0.0;
+	}
+	
+	public List<ReceivableVO> showBusinessRecei(int id,String date) {
+		return null;
+	}
 
 }
