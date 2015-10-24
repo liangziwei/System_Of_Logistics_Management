126ad71b6fe5d29abd9055e69501f9b5b3c840ef
zyfncg <1370305206@qq.com>
2015-10-24 12:59:05 +0800

    github15

--- client/src/main/java/po/businessPO/VehiclePO.java
+++ client/src/main/java/po/businessPO/VehiclePO.java
@@ -2,4 +2,34 @@ package po.businessPO;
 
 public class VehiclePO {
 
+	private String vehicleid;
+	private String number;
+	private String age;
+	
+	public VehiclePO(String vehicleid, String number, String age) {
+		this.vehicleid = vehicleid;
+		this.number = number;
+		this.age = age;
+	}
+	
+	
+	public String getVehicleid() {
+		return vehicleid;
+	}
+	public void setVehicleid(String vehicleid) {
+		this.vehicleid = vehicleid;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
 }
