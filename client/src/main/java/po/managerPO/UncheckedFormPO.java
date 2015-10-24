43d887cbc753142c1e98e6abf8950b8f423478d3
liumingke <1272072141@qq.com>
2015-10-24 20:07:24 +0800

    UncheckedForm定义

--- client/src/main/java/po/managerPO/UncheckedFormPO.java
+++ client/src/main/java/po/managerPO/UncheckedFormPO.java
@@ -1,8 +1,145 @@
 package po.managerPO;
+
+import java.util.List;
+
+import po.businessPO.ArrivalFormPO;
+import po.businessPO.EntruckingPO;
+import po.businessPO.ReceivablePO;
+import po.businessPO.SendFormPO;
+import po.deliveryPO.OrderPO;
+import po.financePO.PaymentPO;
+import po.repositoryPO.InRepositoryPO;
+import po.repositoryPO.OutRepositoryPO;
+import po.transitionPO.ReceivingPO;
+import po.transitionPO.TransferringPO;
+
 /**
  * 记录了未审批单据的信息
  * @author 张仁知
  */
 public class UncheckedFormPO {
+	/**
+	 *未审批寄件单
+	 */
+	private List<OrderPO> orderlist;
+	
+	/**
+	 *未审批装车单 
+	 */
+	private List<EntruckingPO> entruckinglist;
+	/**
+	 *未审批营业厅到达单 
+	 */
+	private List<ArrivalFormPO> arrivalformlist;
+	/**
+	 *未审批收款单 
+	 */
+	private List<ReceivablePO> receivablelist;
+	/**
+	 *未审批派件单
+	 */
+	private List<SendFormPO> sendformlist;
+	/**
+	 *未审批中转中心到达单 
+	 */
+	private List<ReceivingPO> receivinglist;
+	/**
+	 *未审批入库单 
+	 */
+	private List<InRepositoryPO> inrepositorylist;
+	/**
+	 *未审批中转单 
+	 */
+	private List<TransferringPO> transferringlist;
+	/**
+	 *未审批出库单 
+	 */
+	
+    private List<OutRepositoryPO> outrepositorylist;
+	/**
+	 *未审批付款单 
+	 */
+	private List<PaymentPO> paymentlist;
+	public UncheckedFormPO(List<OrderPO> orderlist,
+			List<EntruckingPO> entruckinglist,
+			List<ArrivalFormPO> arrivalformlist,
+			List<ReceivablePO> receivablelist, List<SendFormPO> sendformlist,
+			List<ReceivingPO> receivinglist,
+			List<InRepositoryPO> inrepositorylist,
+			List<TransferringPO> transferringlist,
+			List<OutRepositoryPO> outrepositorylist, List<PaymentPO> paymentlist) {
+		super();
+		this.orderlist = orderlist;
+		this.entruckinglist = entruckinglist;
+		this.arrivalformlist = arrivalformlist;
+		this.receivablelist = receivablelist;
+		this.sendformlist = sendformlist;
+		this.receivinglist = receivinglist;
+		this.inrepositorylist = inrepositorylist;
+		this.transferringlist = transferringlist;
+		this.outrepositorylist = outrepositorylist;
+		this.paymentlist = paymentlist;
+	}
+	public List<OrderPO> getOrderlist() {
+		return orderlist;
+	}
+	public void setOrderlist(List<OrderPO> orderlist) {
+		this.orderlist = orderlist;
+	}
+	public List<EntruckingPO> getEntruckinglist() {
+		return entruckinglist;
+	}
+	public void setEntruckinglist(List<EntruckingPO> entruckinglist) {
+		this.entruckinglist = entruckinglist;
+	}
+	public List<ArrivalFormPO> getArrivalformlist() {
+		return arrivalformlist;
+	}
+	public void setArrivalformlist(List<ArrivalFormPO> arrivalformlist) {
+		this.arrivalformlist = arrivalformlist;
+	}
+	public List<ReceivablePO> getReceivablelist() {
+		return receivablelist;
+	}
+	public void setReceivablelist(List<ReceivablePO> receivablelist) {
+		this.receivablelist = receivablelist;
+	}
+	public List<SendFormPO> getSendformlist() {
+		return sendformlist;
+	}
+	public void setSendformlist(List<SendFormPO> sendformlist) {
+		this.sendformlist = sendformlist;
+	}
+	public List<ReceivingPO> getReceivinglist() {
+		return receivinglist;
+	}
+	public void setReceivinglist(List<ReceivingPO> receivinglist) {
+		this.receivinglist = receivinglist;
+	}
+	public List<InRepositoryPO> getInrepositorylist() {
+		return inrepositorylist;
+	}
+	public void setInrepositorylist(List<InRepositoryPO> inrepositorylist) {
+		this.inrepositorylist = inrepositorylist;
+	}
+	public List<TransferringPO> getTransferringlist() {
+		return transferringlist;
+	}
+	public void setTransferringlist(List<TransferringPO> transferringlist) {
+		this.transferringlist = transferringlist;
+	}
+	public List<OutRepositoryPO> getOutrepositorylist() {
+		return outrepositorylist;
+	}
+	public void setOutrepositorylist(List<OutRepositoryPO> outrepositorylist) {
+		this.outrepositorylist = outrepositorylist;
+	}
+	public List<PaymentPO> getPaymentlist() {
+		return paymentlist;
+	}
+	public void setPaymentlist(List<PaymentPO> paymentlist) {
+		this.paymentlist = paymentlist;
+	}
+	
 
 }
--- client/src/main/java/vo/managerVO/UncheckedFormVO.java
+++ client/src/main/java/vo/managerVO/UncheckedFormVO.java
@@ -1,8 +1,64 @@
 package vo.managerVO;
+
+import java.util.List;
+
+import vo.businessVO.ArrivalFormVO;
+import vo.businessVO.EntruckingVO;
+import vo.businessVO.ReceivableVO;
+import vo.businessVO.SendFormVO;
+import vo.deliveryVO.OrderVO;
+import vo.financeVO.PaymentVO;
+import vo.repositoryVO.InRepositoryVO;
+import vo.repositoryVO.OutRepositoryVO;
+import vo.transitionVO.ReceivingVO;
+import vo.transitionVO.TransferringVO;
+
+
 /**
  * 记录了未审批单据的信息
  * @author 张仁知
  */
 public class UncheckedFormVO {
+	/**
+	 *未审批寄件单
+	 */
+	private List<OrderVO> orderlist;
 	
+	/**
+	 *未审批装车单 
+	 */
+	private List<EntruckingVO> entruckinglist;
+	/**
+	 *未审批营业厅到达单 
+	 */
+	private List<ArrivalFormVO> arrivalformlist;
+	/**
+	 *未审批收款单 
+	 */
+	private List<ReceivableVO> receivablelist;
+	/**
+	 *未审批派件单
+	 */
+	private List<SendFormVO> sendformlist;
+	/**
+	 *未审批中转中心到达单 
+	 */
+	private List<ReceivingVO> receivinglist;
+	/**
+	 *未审批入库单 
+	 */
+	private List<InRepositoryVO> inrepositorylist;
+	/**
+	 *未审批中转单 
+	 */
+	private List<TransferringVO> transferringlist;
+	/**
+	 *未审批出库单 
+	 */
+	
+    private List<OutRepositoryVO> outrepositorylist;
+	/**
+	 *未审批付款单 
+	 */
+	private List<PaymentVO> paymentlist;
 }
