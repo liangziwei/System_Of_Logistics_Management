8c8044c4ef458d2c99cca008d603fea322cce365
liumingke <1272072141@qq.com>
2015-11-07 21:23:18 +0800

    Ok

--- client/src/test/java/mock/test/CostTest.java
+++ client/src/test/java/mock/test/CostTest.java
@@ -8,14 +8,14 @@ import org.junit.Test;
 import businessLogic.businessLogicController.financeController.CostController;
 import businessLogic.businessLogicModel.financeModel.Cost;
 import po.financePO.PaymentPO;
+import vo.financeVO.PaymentVO;
 
 public class CostTest {
 
 	@Test
 	public void test() {
 		CostController costControl = new CostController();
-		
-		
+		PaymentVO paymentVO = new PaymentVO(null, 100.0, null, null, null, null);		
 		assertEquals(true, costControl.addPayment(paymentVO));
 	}
 
