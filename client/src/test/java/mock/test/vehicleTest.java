30da3e9954068a045ffe73e31717c54f1adc1049
zyfncg <1370305206@qq.com>
2015-11-07 16:04:54 +0800

    vehicleTest完善

--- client/src/test/java/mock/test/vehicleTest.java
+++ client/src/test/java/mock/test/vehicleTest.java
@@ -4,7 +4,20 @@ import org.junit.Test;
 
 public class vehicleTest {
 	@Test
+	
 	public void testaddVehicle(){
+		
+	}
+	
+	public void testfindVehicle(){
+		
+	}
+	
+	public void testdeleteVehicle(){
+		
+	}
+	
+	public void testmodifyVehicle(){
 		
 	}
 }
