d583bf333a44267414b45c5c7499efb9626f97af
liumingke <1272072141@qq.com>
2015-11-18 20:08:51 +0800

    账户管理的dataimp

--- server/src/main/java/mysql/DatabaseConnect.java
+++ server/src/main/java/mysql/DatabaseConnect.java
@@ -12,7 +12,7 @@ public class DatabaseConnect {
 
 	private static final String URL="jdbc:mysql://127.0.0.1/logistics_system";
 	private static final String USER="root";
-	private static final String PASSWORD="X1231z60";
+	private static final String PASSWORD="blabla";
 	
 	private static Connection conn=null;
 	
