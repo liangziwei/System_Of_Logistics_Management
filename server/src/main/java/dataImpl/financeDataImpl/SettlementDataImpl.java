52210442ba4e2674235b1b3e8ad6abebfd1acdaa
iaxax <1483809252@qq.com>
2015-12-05 19:45:49 +0800

    完善财务人员

--- server/src/main/java/dataImpl/financeDataImpl/SettlementDataImpl.java
+++ server/src/main/java/dataImpl/financeDataImpl/SettlementDataImpl.java
@@ -1,10 +1,12 @@
 package dataImpl.financeDataImpl;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import dataService.financeDataService.SettlementDataService;
+import mysql.Database;
 import po.businessPO.ReceivablePO;
 
 /**
@@ -16,24 +18,17 @@ public class SettlementDataImpl implements SettlementDataService{
 	ResultSet rs;
 	
 	public List<ReceivablePO> getReceiList(String date) {
-		List<ReceivablePO> receivables = new ArrayList<ReceivablePO>();
-		ArrayList<String> id1 = new ArrayList<String>();
-		id1.add("1000010000");
-		id1.add("1000010001");
-		ArrayList<String> id2 = new ArrayList<String>();
-		id2.add("0000010000");
-		id2.add("0000010001");
-		ArrayList<String> id3 = new ArrayList<String>();
-		id3.add("1000610000");
-		id3.add("1000610001");
-		id3.add("1000610002");
-		id3.add("1000610001");
-		id3.add("1000610002");
-		id3.add("1000610003");
-		receivables.add(new ReceivablePO("1990-1-1", 12.0, "A", id1));
-		receivables.add(new ReceivablePO("1990-1-1", 0.0, "B", id2));
-		receivables.add(new ReceivablePO("1990-1-1", 0.0, "C", id3));
-		return receivables;
+		List<ReceivablePO> po = new ArrayList<ReceivablePO>();
+		String sql = "select * from receivable where date = '" + date + "';";
+		try {
+			ResultSet rs = Database.findOperation(sql);
+			while(rs.next()) {
+				po.add(this.createReceivablePO(rs));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return po;
 	}
 	
 	public List<ReceivablePO> getBusinessRecei(String id,String date) {
@@ -80,7 +75,21 @@ public class SettlementDataImpl implements SettlementDataService{
 		return receivables;
 	}
 	
-//	private ReceivablePO rsToReceivablePO(ResultSet rs){
-//		
-//	}
+	private ReceivablePO createReceivablePO(ResultSet rs) {
+		ArrayList<String> id = new ArrayList<String>();;
+		try {
+			String idText = rs.getString("deliveryid");
+			String[] temp = idText.split(" ");
+			int size = temp.length;
+			for(int i = 0; i < size; i++) {
+				id.add(temp[i]);
+			}
+			return new ReceivablePO(rs.getString("date"), rs.getDouble("money"),
+					rs.getString("courier"), id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
