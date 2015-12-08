d2d196bdbae1ca335efef6c3114201808300052c
Sam7rww <1543169449@qq>
2015-12-09 00:08:15 +0800

    test最后一次修改

--- server/src/test/java/businessTest/EntruckingDataTest.java
+++ server/src/test/java/businessTest/EntruckingDataTest.java
@@ -1,6 +1,6 @@
 package businessTest;
 
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -9,11 +9,11 @@ import po.businessPO.EntruckingPO;
 
 public class EntruckingDataTest {
 
-	@Test
-	public void test() {
-		EntruckingPO entruckingPO=new EntruckingPO("2015-10-10","025001","0250012015101012345","025002","025001001","监装员","押运员",600);
-		EntruckingDataImpl entruckingData=new EntruckingDataImpl();
-		assertEquals(true, entruckingData.addEntruckingFrom(entruckingPO));
-	}
+//	@Test
+//	public void test() {
+//		EntruckingPO entruckingPO=new EntruckingPO("2015-10-10","025001","0250012015101012345","025002","025001001","监装员","押运员",600);
+//		EntruckingDataImpl entruckingData=new EntruckingDataImpl();
+//		assertEquals(true, entruckingData.addEntruckingFrom(entruckingPO));
+//	}
 
 }
