30be818dcf0f761c1b1da9296bd5e55bba741dd9
Sam7rww <1543169449@qq>
2015-12-08 19:19:45 +0800

    Jekins

--- client/src/main/java/ui/repositoryui/outRep/SeeTransferringListPanel.java
+++ client/src/main/java/ui/repositoryui/outRep/SeeTransferringListPanel.java
@@ -1,33 +1,27 @@
 package ui.repositoryui.outRep;
 
 import java.awt.Color;
-import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import businessLogic.businessLogicController.repositoryController.OutRepositoryController;
 import businessLogic.businessLogicModel.util.CommonLogic;
 import businessLogicService.repositoryBLService.OutRepositoryBLService;
-import constant.AreaCodeType;
 import constant.LoadingType;
 import ui.DateChooser;
 import ui.baseui.DetailPanel;
 import ui.baseui.LimpidButton;
 import ui.transitionui.loadingui.AddLoadingPanel;
-import vo.businessVO.ReceivableVO;
-import vo.repositoryVO.RepositoryVO;
 import vo.transitionVO.TransferringVO;
 
 public class SeeTransferringListPanel extends DetailPanel{
@@ -38,10 +32,10 @@ public class SeeTransferringListPanel extends DetailPanel{
 	private JTable table = null;
 	private JLabel time = new JLabel("查询时间");
 	private JTextField timeyear = new JTextField();
-	private JTextField timemonth = new JTextField();
-	private JTextField timeday = new JTextField();
-	private JLabel apart1 = new JLabel("-");
-	private JLabel apart2 = new JLabel("-");
+//	private JTextField timemonth = new JTextField();
+//	private JTextField timeday = new JTextField();
+//	private JLabel apart1 = new JLabel("-");
+//	private JLabel apart2 = new JLabel("-");
 	private LimpidButton find = new LimpidButton("","picture/查询.png");
 
 	private LimpidButton cancle = new LimpidButton("","picture/取消.png");
@@ -88,16 +82,14 @@ public class SeeTransferringListPanel extends DetailPanel{
 		//组件
 		time.setBounds(50, 10, LABEL_W, LABEL_H);
 		this.add(time);
-		find.setBounds(timeday.getX()+timeday.getWidth()+COMPONENT_GAP_X+10, timeday.getY(), LABEL_W+20, LABEL_H);
 		timeyear.setBounds(time.getX()+time.getWidth()+COMPONENT_GAP_X, time.getY(), (TEXT_W/2)*3, TEXT_H);
 		timeyear.setOpaque(false);
 		dateChoose.register(timeyear);
 		this.add(timeyear);
 
-		find.setBounds(timeyear.getX()+timeyear.getWidth()+COMPONENT_GAP_X+30, timeyear.getY(), LABEL_W+20, LABEL_H);
-		find.setBounds(timeyear.getX()+timeyear.getWidth()+COMPONENT_GAP_X+30, timeyear.getY(), LABEL_W+20, LABEL_H);
+		find.setBounds(timeyear.getX()+timeyear.getWidth()+COMPONENT_GAP_X+30, timeyear.getY(), LABEL_W, LABEL_H-10);
 		this.add(find);
-		cancle.setBounds(find.getX()+find.getWidth()+COMPONENT_GAP_X+10, find.getY(), LABEL_W+20, LABEL_H);
+		cancle.setBounds(find.getX()+find.getWidth()+COMPONENT_GAP_X+10, find.getY(), LABEL_W, LABEL_H-10);
 		this.add(cancle);
 		state.setBounds(time.getX(), time.getY()+time.getHeight(), AddLoadingPanel.DETAIL_PANEL_W, LABEL_H);
 		state.setForeground(Color.red);
