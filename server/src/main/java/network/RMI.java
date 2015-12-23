07210201d0106b9275ad61554189699dbcd335f3
iaxax <1483809252@qq.com>
2015-12-23 23:21:16 +0800

    将RMI绑定封装为一个方法，降低代码重复

--- server/src/main/java/network/RMI.java
+++ server/src/main/java/network/RMI.java
@@ -1,9 +1,12 @@
 package network;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
 
 import dataImpl.NetworkTestImpl;
 import dataImpl.administratorDataImpl.AdministratorDataImpl;
@@ -17,7 +20,6 @@ import dataImpl.deliveryDataImpl.ReceiptDataImpl;
 import dataImpl.financeDataImpl.AccountDataImpl;
 import dataImpl.financeDataImpl.CostDataImpl;
 import dataImpl.financeDataImpl.OriginalInfoDataImpl;
-import dataImpl.financeDataImpl.OriginalInfoRecordDataImpl;
 import dataImpl.financeDataImpl.SettlementDataImpl;
 import dataImpl.financeDataImpl.StatisticsDataImpl;
 import dataImpl.managerDataImpl.ApprovalFormDataImpl;
@@ -31,239 +33,109 @@ import dataImpl.senderDataImpl.InquireDataImpl;
 import dataImpl.transitionDataImpl.LoadingDataImpl;
 import dataImpl.transitionDataImpl.ReceivingDataImpl;
 import dataImpl.transitionDataImpl.TransferringDataImpl;
-import dataService.NetworkTest;
-import dataService.administratorDataService.AdministratorDataService;
-import dataService.businessDataService.DriverDataService;
-import dataService.businessDataService.EntruckingDataService;
-import dataService.businessDataService.PaymentDataService;
-import dataService.businessDataService.ReceiveAndSendDataService;
-import dataService.businessDataService.VehicleDataService;
-import dataService.deliveryDataService.OrderDataService;
-import dataService.deliveryDataService.ReceiptDataService;
-import dataService.financeDataService.AccountDataService;
-import dataService.financeDataService.CostDataService;
-import dataService.financeDataService.OriginalInfoDataService;
-import dataService.financeDataService.SettlementDataService;
-import dataService.financeDataService.StatisticsDataService;
-import dataService.managerDataService.ApprovalFormDataService;
-import dataService.managerDataService.MakeConstantDataService;
-import dataService.managerDataService.OrganizationManagementDataService;
-import dataService.managerDataService.StaffManagementDataService;
-import dataService.repositoryDataService.InRepositoryDataService;
-import dataService.repositoryDataService.ManageRepositoryDataService;
-import dataService.repositoryDataService.OutRepositoryDataService;
-import dataService.senderDataService.InquireDataService;
-import dataService.transitionDataService.LoadingDataService;
-import dataService.transitionDataService.ReceivingDataService;
-import dataService.transitionDataService.TransferringDataService;
 
 public class RMI {
+	/**
+	 *远程服务注册对象 
+	 */
+	private static Registry REGISTRY = null;
 	
-	private static Registry registry = null;
+	/**
+	 *远程服务对象列表 
+	 */
+	private static List<RemoteObjectMap> REMOTE_MAP_LIST = new ArrayList<RemoteObjectMap>();
 	
 	static {
 		try {
-			registry = LocateRegistry.createRegistry(1099);
+			REGISTRY = LocateRegistry.createRegistry(1099);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
+		//收件人
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("inquire", new InquireDataImpl()));
+		//寄件人
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("order", new OrderDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("receipt", new ReceiptDataImpl()));
+		//营业厅业务员
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("entrucking", new EntruckingDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("receivable", new PaymentDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("receiveAndSend", new ReceiveAndSendDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("vehicle", new VehicleDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("driver", new DriverDataImpl()));
+		//中转中心仓库管理人员
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("inrepository", new InRepositoryDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("outrepository", new OutRepositoryDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("manage", new ManageRepositoryDataImpl()));
+		//中转中心业务员
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("loading", new LoadingDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("receiving", new ReceivingDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("transferring", new TransferringDataImpl()));
+		//总经理
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("approvalForm", new ApprovalFormDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("makeConstant", new MakeConstantDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("organization", new OrganizationManagementDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("staff", new StaffManagementDataImpl()));
+		//财务人员
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("account", new AccountDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("cost", new CostDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("originalInfo", new OriginalInfoDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("settlement", new SettlementDataImpl()));
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("statistics", new StatisticsDataImpl()));
+		//系统管理人员
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("administrator", new AdministratorDataImpl()));
+		//网络连接测试
+		REMOTE_MAP_LIST.add(new RemoteObjectMap("test", new NetworkTestImpl()));
 	}
 
 	public static void initRMI() {
-		//初始化寄件人的RMI连接
-		initSenderRMI();
-		//初始化快递员的RMI连接
-		initDeliveryRMI();
-		//初始化营业厅业务员的RMI连接
-		initBusinessRMI();
-		//初始化中装中心库存管理人员的RMI连接
-		initRepositoryRMI();
-		//初始化中转中心业务员的RMI连接
-		initTransitionRMI();
-		//初始化总经理的RMI连接
-		initManagerRMI();
-		//初始化财务人员的RMI连接
-		initFinanceRMI();
-		//初始化系统管理人员的RMI连接
-		initAdministratorRMI();
-		//初始化网络测试的RMI连接
-		initTestRMI();
+		for (RemoteObjectMap map : REMOTE_MAP_LIST) {
+			bind(map.getService(), map.getKey());
+		}
 		//提示服务器运行成功
 		System.out.println("Server is working...");
 	}
 	
-	private static void initTestRMI() {
+	/**
+	 * 将远程服务对象与相应字符串绑定
+	 * @param implementation， 远程服务接口的具体实现对象
+	 * @param key， 远程服务对象对应的关键字
+	 */
+	private static<Service extends Remote> void bind(Service implementation, String key) {
 		try {
-			NetworkTest test = new NetworkTestImpl();
-			NetworkTest stub = (NetworkTest) UnicastRemoteObject.exportObject(test, 0);
-			registry.bind("test", stub);		
+			@SuppressWarnings("unchecked")
+			Service stub = (Service) UnicastRemoteObject.exportObject(implementation, 0);
+			REGISTRY.bind(key, stub);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+}
 
-	private static void initSenderRMI() {
-		try {
-			//物流信息访问接口
-			InquireDataService inquire = new InquireDataImpl();
-			InquireDataService stub = (InquireDataService) UnicastRemoteObject.exportObject(inquire, 0);
-			registry.bind("inquire", stub);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static void initDeliveryRMI() {
-		try {
-			//订单数据访问接口
-			OrderDataService order = new OrderDataImpl();
-			OrderDataService order_stub = (OrderDataService) UnicastRemoteObject.exportObject(order, 0);
-			registry.bind("order", order_stub);
-			//收件信息数据访问接口
-			ReceiptDataService receipt = new ReceiptDataImpl();
-			ReceiptDataService receipt_stub = (ReceiptDataService) UnicastRemoteObject.exportObject(receipt, 0);
-			registry.bind("receipt", receipt_stub);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+/**
+ * 远程服务对象映射，包含远程服务对象与对应的关键字
+ */
+class RemoteObjectMap {
+	
+	private String key;
+	
+	private Remote service;
+	
+	public RemoteObjectMap(String key, Remote service) {
+		this.key = key;
+		this.service = service;
 	}
 	
-	private static void initBusinessRMI() {
-		
-		try {
-			EntruckingDataService entrucking = new EntruckingDataImpl();
-			EntruckingDataService entrucking_stub = (EntruckingDataService) UnicastRemoteObject.exportObject(entrucking, 0);
-			registry.bind("entrucking", entrucking_stub);
-			
-			PaymentDataService payment = new PaymentDataImpl();
-			PaymentDataService payment_stub = (PaymentDataService) UnicastRemoteObject.exportObject(payment, 0);
-			registry.bind("receivable", payment_stub);
-			
-			ReceiveAndSendDataService receiveAndSend = new ReceiveAndSendDataImpl();
-			ReceiveAndSendDataService receiveAndSend_stub = (ReceiveAndSendDataService) UnicastRemoteObject.exportObject(receiveAndSend, 0);
-			registry.bind("receiveAndSend", receiveAndSend_stub);
-			
-			VehicleDataService vehicle = new VehicleDataImpl();
-			VehicleDataService vehicle_stub = (VehicleDataService) UnicastRemoteObject.exportObject(vehicle, 0);
-			registry.bind("vehicle", vehicle_stub);
-			
-			DriverDataService driver = new DriverDataImpl();
-			DriverDataService driver_stub = (DriverDataService) UnicastRemoteObject.exportObject(driver, 0);
-			registry.bind("driver", driver_stub);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+	/**
+	 *获得与对象绑定的关键字 
+	 */
+	public String getKey() {
+		return this.key;
 	}
 	
-	private static void initRepositoryRMI() {
-		try {
-			//入库单访问借口
-			InRepositoryDataService inRepository = new InRepositoryDataImpl();
-			InRepositoryDataService inRepository_stub = (InRepositoryDataService) UnicastRemoteObject.exportObject(inRepository,0);
-			registry.bind("inrepository", inRepository_stub);
-			//出库单访问借口
-			OutRepositoryDataService outRepository = new OutRepositoryDataImpl();
-			OutRepositoryDataService outRepository_stub = (OutRepositoryDataService) UnicastRemoteObject.exportObject(outRepository,0);
-			registry.bind("outrepository", outRepository_stub);
-			//库存管理访问借口
-			ManageRepositoryDataService manage = new ManageRepositoryDataImpl();
-			ManageRepositoryDataService manage_stub = (ManageRepositoryDataService) UnicastRemoteObject.exportObject(manage, 0);
-			registry.bind("manage", manage_stub);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	/**
+	 *获得远程服务对象 
+	 */
+	public Remote getService() {
+		return this.service;
 	}
-	
-	private static void initTransitionRMI() {
-		try {
-			//装运单访问借口
-			LoadingDataService loading = new LoadingDataImpl();
-			LoadingDataService loading_stub = (LoadingDataService) UnicastRemoteObject.exportObject(loading,0);
-			registry.bind("loading", loading_stub);
-			//接收单访问借口
-			ReceivingDataService receiving = new ReceivingDataImpl();
-			ReceivingDataService receiving_stub = (ReceivingDataService) UnicastRemoteObject.exportObject(receiving,0);
-			registry.bind("receiving", receiving_stub);
-			//中转单访问借口
-			TransferringDataService transferring = new TransferringDataImpl();
-			TransferringDataService transferring_stub = (TransferringDataService) UnicastRemoteObject.exportObject(transferring,0);
-			registry.bind("transferring", transferring_stub);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	private static void initManagerRMI() {
-		try{
-			//审批单据访问接口
-			ApprovalFormDataService approvalForm = new ApprovalFormDataImpl();
-			ApprovalFormDataService approvalForm_stub = (ApprovalFormDataService) UnicastRemoteObject.exportObject(approvalForm, 0);
-			registry.bind("approvalForm", approvalForm_stub);
-			//城市距离和价格常量访问接口
-			MakeConstantDataService makeConstant = new MakeConstantDataImpl();
-			MakeConstantDataService makeConstant_stub = (MakeConstantDataService)UnicastRemoteObject.exportObject(makeConstant, 0);
-			registry.bind("makeConstant", makeConstant_stub);
-			//机构信息访问接口
-			OrganizationManagementDataService organization = new OrganizationManagementDataImpl();
-			OrganizationManagementDataService organization_stub = (OrganizationManagementDataService)UnicastRemoteObject.exportObject(organization, 0);
-			registry.bind("organization", organization_stub);
-			//人员信息访问接口
-			StaffManagementDataService staff = new StaffManagementDataImpl();
-			StaffManagementDataService staff_stub = (StaffManagementDataService) UnicastRemoteObject.exportObject(staff, 0);
-			registry.bind("staff", staff_stub);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-
-	}
-	
-	private static void initFinanceRMI() {
-		try{
-			//账号信息访问接口
-			AccountDataService account = new AccountDataImpl();
-			AccountDataService account_stub = (AccountDataService) UnicastRemoteObject.exportObject(account, 0);
-			registry.bind("account", account_stub);
-			//成本信息访问接口
-			CostDataService cost = new CostDataImpl();
-			CostDataService cost_stub = (CostDataService) UnicastRemoteObject.exportObject(cost, 0);
-			registry.bind("cost", cost_stub);
-			//期初建账访问接口
-			OriginalInfoDataService originalInfo = new OriginalInfoDataImpl();
-			OriginalInfoDataService originalInfo_stub =
-					(OriginalInfoDataService) UnicastRemoteObject.exportObject(originalInfo, 0);
-			registry.bind("originalInfo", originalInfo_stub);
-			
-			//建账查看访问接口
-			OriginalInfoDataService originalInfoRecord = new OriginalInfoRecordDataImpl();
-			OriginalInfoDataService originalInfoRecord_stub =
-					(OriginalInfoDataService) UnicastRemoteObject.exportObject(originalInfoRecord, 0);
-			registry.bind("originalInfoRecord", originalInfoRecord_stub);
-			//结算管理访问接口
-			SettlementDataService settlement = new SettlementDataImpl();
-			SettlementDataService settlement_stub = (SettlementDataService) UnicastRemoteObject.exportObject(settlement, 0);
-			registry.bind("settlement", settlement_stub);
-			//统计分析访问接口
-			StatisticsDataService statistics = new StatisticsDataImpl();
-			StatisticsDataService statistics_stub = (StatisticsDataService) UnicastRemoteObject.exportObject(statistics, 0);
-			registry.bind("statistics", statistics_stub);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	private static void initAdministratorRMI() {
-		try {
-			//用户信息访问接口
-			AdministratorDataService admin = new AdministratorDataImpl();
-			AdministratorDataService stub = (AdministratorDataService) UnicastRemoteObject.exportObject(admin, 0);
-			registry.bind("administrator", stub);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 }
